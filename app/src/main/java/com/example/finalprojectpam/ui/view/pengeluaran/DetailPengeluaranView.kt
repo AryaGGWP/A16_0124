80510fb00850651f0af1832fb71ab7d337269cbe
AryaGGWP <arya.lampung.2016@gmail.com>
2025-01-28 21:08:38 +0700

    add view untuk halaman detail pengeluaran

---
+++ app/src/main/java/com/example/finalprojectpam/ui/view/pengeluaran/DetailPengeluaranView.kt
@@ -0,0 +1,8 @@
+package com.example.finalprojectpam.ui.view.pengeluaran
+
+import com.example.finalprojectpam.ui.navigation.DestinasiNavigasi
+
+object DestinasiPengeluaranPage : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home"
+}
