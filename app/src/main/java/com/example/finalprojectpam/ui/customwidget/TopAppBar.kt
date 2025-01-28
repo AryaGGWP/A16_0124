955b408e1968ffebfd7876ae8a8199f187e2d6c9
AryaGGWP <arya.lampung.2016@gmail.com>
2025-01-28 20:56:16 +0700

    add TopAppBar

--- app/src/main/java/com/example/finalprojectpam/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/finalprojectpam/ui/customwidget/TopAppBar.kt
@@ -25,7 +25,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -39,11 +38,10 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.finalprojectpam.ui.viewmodel.HomeViewModel
-import com.example.pertemuan12.R
+import com.example.finalprojectpam.ui.viewmodel.Home.HomeViewModel
+import com.example.finalprojectpam.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
