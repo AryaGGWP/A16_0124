9f8555f5602278bb7fc93330f203cd544b9cfb65
AryaGGWP <arya.lampung.2016@gmail.com>
2025-01-28 21:43:21 +0700

    memanggil KeuanganApp

--- app/src/main/java/com/example/finalprojectpam/MainActivity.kt
+++ app/src/main/java/com/example/finalprojectpam/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.finalprojectpam.ui.theme.FinalprojectpamTheme
+import com.example.finalprojectpam.ui.viewmodel.KeuanganApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             FinalprojectpamTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                 KeuanganApp(
+                     modifier = Modifier.padding(innerPadding)
+                 )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FinalprojectpamTheme {
-        Greeting("Android")
     }
 }
